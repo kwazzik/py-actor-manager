f3ff36683b5a3f75167834e76746840f3f81b064
Misha Patkanovtsi <mishakwazzi@gmail.com>
2025-08-13 21:45:23 +0300

    Solution

--- app/managers.py
+++ app/managers.py
@@ -11,7 +11,7 @@ class ActorManager:
 
     def create(self, first_name: str, last_name: str) -> None:
         cursor = self._connection.cursor()
-        cursor.execute("INSERT INTO Actor (first_name, last_name) VALUES "
+        cursor.execute(f"INSERT INTO {self.table_name} (first_name, last_name) VALUES "
                        "(?, ?)",
                        (first_name, last_name)
                        )
@@ -19,21 +19,21 @@ class ActorManager:
 
     def all(self) -> list[Actor]:
         cursor = self._connection.cursor()
-        cursor.execute("SELECT * FROM Actor")
+        cursor.execute(f"SELECT * FROM {self.table_name}")
         rows = cursor.fetchall()
         return [Actor(id=row[0], first_name=row[1],
                       last_name=row[2]) for row in rows]
 
     def update(self, pk: int, new_first_name: str, new_last_name: str) -> None:
         cursor = self._connection.cursor()
-        cursor.execute("UPDATE Actor SET first_name = ?, "
+        cursor.execute(f"UPDATE {self.table_name} SET first_name = ?, "
                        "last_name = ? WHERE id = ?",
                        (new_first_name, new_last_name, pk))
         self._connection.commit()
 
     def delete(self, pk: int) -> None:
         cursor = self._connection.cursor()
-        cursor.execute("DELETE FROM Actor WHERE id = ?",
+        cursor.execute(f"DELETE FROM {self.table_name} WHERE id = ?",
                        (pk,)
                        )
         self._connection.commit()
